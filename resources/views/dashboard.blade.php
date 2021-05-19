27a2d951bf65c35e3daa284f449786e19375b595
natchez <natchezzz@hotmail.com>
2021-05-19 11:15:49 +0200

    fin  ajout variable admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,15 +12,27 @@
                 <div class="my-5 bg-white py-8 px-10 text-center rounded-md shadow-lg max-w-xs mx-auto">
                     <h2 class="font-semibold text-2xl mb-6">{{$user->name}}</h2>
                     <img class="w-40 h-40 object-cover rounded-full mx-auto shadow-lg"
-                        src={{asset($user->avatar->name)}}
-                        alt="User avatar">
-                    <p class="capitalize text-xl mt-1 ">{{$user->age}}  ans</p>
+                        src={{asset($user->avatar->name)}} alt="User avatar">
+                    <p class="capitalize text-xl mt-1 ">{{$user->age}} ans</p>
                     <span class="flex items-center border rounded-full w-24 pr-2 justify-center mx-auto mt-2 mb-12">
                         <div class="bg-green-400 rounded-full w-2.5 h-2.5 block mr-2"></div>{{$user->role->name}}
                     </span>
-                    <button
-                        class="rounded-md bg-gradient-to-r from-blue-400 to-indigo-500 text-xl text-white pt-3 pb-4 px-8 inline">Send
-                        a message</button>
+                    <div class="flex">
+                        @admin
+                        <button
+                            class="rounded-md bg-gradient-to-r from-blue-400 to-indigo-500 text-xl text-white pt-3 pb-4 px-4 inline">Modifier
+                        </button>
+                        @if (Auth::user()->id == $user->id)
+                        <button
+                            class="rounded-md bg-gradient-to-r from-green-400 to-green-500 text-xl text-white pt-3 pb-4 px-4 inline">Actif
+                        </button>
+                        @else
+                        <button
+                            class="rounded-md bg-gradient-to-r from-red-400 to-red-500 text-xl text-white pt-3 pb-4 px-4 inline">supprimer
+                        </button>
+                        @endif
+                        @endadmin
+                    </div>
                 </div>
                 @endforeach
             </div>
